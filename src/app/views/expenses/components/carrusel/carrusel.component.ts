4b414950c7e5d227cadef4c2013f0aac46b9aad4
Duvan Salgado <dsalgado012@gmail.com>
2024-06-10 11:30:44 -0500

    footer compartido

---
+++ src/app/views/expenses/components/carrusel/carrusel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'finance-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class CarruselCompone {}
